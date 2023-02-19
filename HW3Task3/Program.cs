0b8d0c9ae7a2572587f0b7341e813d87dd7d13dc
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-02-19 14:19:26 +0300

    Task 3

---
+++ HW3Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}> ");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int number = InputInt("Введите число N, до которого выводим таблицу кубов: ");
+for (int i = 1; i <= number; i++)
+{
+    int cube = i * i * i;
+    System.Console.Write($"{cube} ");
+}
